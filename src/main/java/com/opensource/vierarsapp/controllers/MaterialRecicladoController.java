3bb141b7cadfbe73f7a8b0985b30722c743e2d5d
carlostarazona <carlostarazona887@hotmail.com>
2019-11-30 02:21:20 -0500

    hola

--- src/main/java/com/opensource/vierarsapp/controllers/MaterialRecicladoController.java
+++ src/main/java/com/opensource/vierarsapp/controllers/MaterialRecicladoController.java
@@ -2,11 +2,19 @@ package com.opensource.vierarsapp.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +37,45 @@ public class MaterialRecicladoController {
 			return new ResponseEntity<List<MateriaReciclado>>(HttpStatus.NO_CONTENT);
 		}
 		return new ResponseEntity<List<MateriaReciclado>>(materialesreciclados, HttpStatus.OK);
+	}
+	
+	@GetMapping("/registrarMaterialReciclado")
+	public void registrarMaterialReciclado(Model model) {
+		
+		MateriaReciclado materialreciclado = new MateriaReciclado();	
+		model.addAttribute("titulo", "Registrar MaterialReciclado");
+		model.addAttribute("reciclador", materialreciclado);
+	}
+	
+	@PostMapping("/guardarMaterialReciclado")
+	public String guardar(@ModelAttribute MateriaReciclado materialreciclado) {
+		_materialrecicladoService.insert(materialreciclado);
+		return "redirect:/";
+	}
+	
+	@GetMapping(value = "/escogerTipoMaterialReciclado")
+	public String escogerTipoMaterialReciclado() {		
+		return "/views/materialreciclado/escogerTipoMaterialReciclado";
+	}
+
+	@RequestMapping(value = "/insertar", method = RequestMethod.POST)
+	public ResponseEntity<String> persistPerson(@RequestBody MateriaReciclado t) {
+	  if (_materialrecicladoService.insert(t)) {
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	  }
+	  return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
+	}
+
+	//Registrar
+	@RequestMapping(value = "/{usuarioID}/{tipoID}/insertar", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public MateriaReciclado crearMaterialReciclado(@PathVariable(value="tipoID") int tipoID, @PathVariable(value="usuarioID") int usuarioID,@RequestBody MateriaReciclado t) {
+	  return _materialrecicladoService.insert(t, usuarioID,tipoID);
+	}
+
+
+	@GetMapping("/{id}")
+	public MateriaReciclado obtenerMaterialReciclado(@PathVariable int id) {
+	Optional<MateriaReciclado> materialreciclado = _materialrecicladoService.finbyId(id);
+	return materialreciclado.get();
 	}
 }
--- src/main/java/com/opensource/vierarsapp/services/MaterialRecicladoService.java
+++ src/main/java/com/opensource/vierarsapp/services/MaterialRecicladoService.java
@@ -6,8 +6,15 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.opensource.vierarsapp.dao.IDistritoRepository;
 import com.opensource.vierarsapp.dao.IMaterialRecicladoRepository;
+import com.opensource.vierarsapp.dao.ITipoRepository;
+import com.opensource.vierarsapp.dao.IUsuarioRepository;
+import com.opensource.vierarsapp.models.Distrito;
 import com.opensource.vierarsapp.models.MateriaReciclado;
+import com.opensource.vierarsapp.models.Tipo;
+import com.opensource.vierarsapp.models.Usuario;
 
 @Service("materialrecicladoService")
 @Transactional
@@ -16,6 +23,11 @@ public class MaterialRecicladoService implements IMaterialRecicladoService{
 	@Autowired
 	private IMaterialRecicladoRepository _materialrecicladoRepository;
 
+	@Autowired
+	private IUsuarioRepository _usuarioRepository;
+
+	@Autowired
+	private ITipoRepository _tipoRepository;
 	@Override
 	public boolean insert(MateriaReciclado t) {
 		try {
@@ -24,6 +36,16 @@ public class MaterialRecicladoService implements IMaterialRecicladoService{
 		} catch (Exception e) {
 			return false;
 		}
+	}
+	
+public MateriaReciclado insert( MateriaReciclado t, int idUsuario,int idTipo){
+		
+		Usuario usuario = _usuarioRepository.findById(idUsuario).get();
+		Tipo tipo = _tipoRepository.findById(idTipo).get();
+		t.setUsuario(usuario);
+		t.setTipo(tipo);
+		_materialrecicladoRepository.save(t);
+		return t;
 	}
 
 	@Override
