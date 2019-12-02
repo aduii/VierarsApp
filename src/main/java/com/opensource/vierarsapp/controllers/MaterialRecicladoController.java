b26f47b943affc6871681051dd7d2b6bd6325fba
AndyGeek <andy.eulogio@gmail.com>
2019-12-01 20:56:34 -0500

    update project

--- src/main/java/com/opensource/vierarsapp/controllers/MaterialRecicladoController.java
+++ src/main/java/com/opensource/vierarsapp/controllers/MaterialRecicladoController.java
@@ -12,12 +12,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.opensource.vierarsapp.models.MateriaReciclado;
+import com.opensource.vierarsapp.models.Tipo;
+import com.opensource.vierarsapp.models.Usuario;
 import com.opensource.vierarsapp.services.MaterialRecicladoService;
+import com.opensource.vierarsapp.services.TipoService;
+import com.opensource.vierarsapp.services.UsuarioService;
 
 @RestController
 @RequestMapping("/materialreciclado")
@@ -25,6 +30,12 @@ public class MaterialRecicladoController {
 
 	@Autowired
 	MaterialRecicladoService _materialrecicladoService;
+
+	@Autowired
+	UsuarioService _usuarioService;
+
+	@Autowired
+	TipoService _tipoService;
 	
 	@RequestMapping(value = "/listar", method = RequestMethod.GET, headers = "Accept=application/json")
 	public ResponseEntity<List<MateriaReciclado>> getStudent(){
@@ -98,6 +109,16 @@ public class MaterialRecicladoController {
 			return new ResponseEntity<List<MateriaReciclado>>(HttpStatus.NO_CONTENT);
 		}
 		return new ResponseEntity<List<MateriaReciclado>>(materialesreciclados, HttpStatus.OK);
+	}
+
+
+	@PutMapping("/{usuarioID}/{tipoID}/editar")
+	public void update(@PathVariable(value="tipoID") int tipoID, @PathVariable(value="usuarioID") int usuarioID, @RequestBody MateriaReciclado materiaReciclado) {
+		Usuario user = _usuarioService.finbyId(usuarioID).get();
+		Tipo tipo  = _tipoService.finbyId(tipoID).get();
+		materiaReciclado.setUsuario(user);
+		materiaReciclado.setTipo(tipo);
+		_materialrecicladoService.update(materiaReciclado);
 	}
 
 
